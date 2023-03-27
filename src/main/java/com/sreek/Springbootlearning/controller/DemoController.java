95c9f3043d25fbf4530dda45655249dd421001a0
Sreekar Swarnapuri <sreekar.swarnapuri@outlook.com>
2023-03-27 13:52:14 +0530

    Added details for logging that can be used with Spring boot directly.

---
+++ src/main/java/com/sreek/Springbootlearning/controller/DemoController.java
@@ -0,0 +1,26 @@
+package com.sreek.Springbootlearning.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class DemoController {
+
+    private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+    @GetMapping("/hello")
+    public Map<String, String> hello(@RequestParam(defaultValue = "World") String name) {
+
+        logger.info("Name: {}", name);
+        logger.trace("Name2: {}", name);
+
+        return Map.of("greeting", "Hello " + name   );
+
+    }
+
+}
